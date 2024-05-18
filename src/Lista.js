5bfe05de21474c15b1f0a8a5b00faf7276803585
Danie0822 <20190652@ricaldone.edu.sv>
2024-05-18 11:44:55 -0600

    Arreglos de las comidas Fav

--- src/Lista.js
+++ src/Lista.js
@@ -1,27 +1,36 @@
 import React, { useState } from 'react';
-import { StyleSheet, Text, View, TouchableOpacity, FlatList, SafeAreaView } from 'react-native';
+import { StyleSheet, Text, View, TouchableOpacity, FlatList, SafeAreaView, Image } from 'react-native';
 
 function ListaComidas({ navigation }) {
   const [comidaSeleccionada, setComidaSeleccionada] = useState('');
   const [comidas, setComidas] = useState([]);
-  const opcionesComidas = ['Pizza', 'Hamburguesa', 'Pollo', 'Lasaña', 'Tacos', 'Carne Asada'];
+  const opcionesComidas = [
+    { nombre: 'Pizza', descripcion: 'Pizza es god ', imagen: require('../assets/pizza.jpeg') },
+    { nombre: 'Hamburguesa', descripcion: 'Quien no le gusta la hamburguesas', imagen: require('../assets/hamburguesa.jpeg') },
+    { nombre: 'Pollo', descripcion: 'pollo para las prote', imagen: require('../assets/pollo.jpeg') },
+    { nombre: 'Lasaña', descripcion: 'Lasaña de italia', imagen: require('../assets/lasaña.jpeg') },
+    { nombre: 'Carne', descripcion: 'Carne Asada', imagen: require('../assets/Carne.jpeg') },
+    { nombre: 'Tacos', descripcion: 'Tacos god ', imagen: require('../assets/tacos.jpeg') },
+  ];
 
   return (
     <SafeAreaView style={styles.contenedor}>
       <Text style={styles.titulo}>Lista de Comidas Favoritas</Text>
-      <View style={styles.contenedorInput}>
-      </View>
+      <View style={styles.contenedorInput}></View>
       <FlatList
         data={opcionesComidas}
         renderItem={({ item }) => (
           <TouchableOpacity
-            style={[styles.contenedorComida, item === comidaSeleccionada ? styles.comidaSeleccionada : null]}
-            onPress={() => setComidaSeleccionada(item)}
+            style={[styles.contenedorComida, item.nombre === comidaSeleccionada ? styles.comidaSeleccionada : null]}
+            onPress={() => setComidaSeleccionada(item.nombre)}
           >
-            <Text style={styles.textoComida}>{item}</Text>
+            <Image source={item.imagen} style={styles.imagenComida} />
+            <View style={{ marginLeft: 10 }}>
+              <Text style={styles.textoComida}>{item.nombre}</Text>
+              <Text style={styles.descripcionComida}>{item.descripcion}</Text>
+            </View>
           </TouchableOpacity>
         )}
-        
         keyExtractor={(item, index) => index.toString()}
       />
       <Text style={styles.comidaSeleccionadaTexto}>Comida seleccionada: {comidaSeleccionada}</Text>
@@ -72,6 +81,10 @@ const styles = StyleSheet.create({
     fontSize: 18,
     color: 'black',
   },
+  descripcionComida: {
+    fontSize: 14,
+    color: 'gray',
+  },
   comidaSeleccionada: {
     backgroundColor: 'yellow',
   },
@@ -80,5 +93,10 @@ const styles = StyleSheet.create({
     fontSize: 18,
     fontWeight: 'bold',
     textAlign: 'center',
+  },
+  imagenComida: {
+    width: 50,
+    height: 50,
+    borderRadius: 25,
   },
 });
